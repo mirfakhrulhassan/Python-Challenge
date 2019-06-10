bed9e25138464701dd982904e9ce15bce380d3f5
mirfakhrulhassan <50125428+mirfakhrulhassan@users.noreply.github.com>
2019-06-10 01:44:08 -0400

    increas/decrease
    increas/decrease

--- PyBank/main.py
+++ PyBank/main.py
@@ -4,6 +4,9 @@ import csv
 month_count = 0
 total_amount = 0.00
 average_change = 0
+profit_loss = []
+average_chnge = []
+dateList=[]
 
 flpth = os.path.join("D:\\UOFT DATA ANALYTICS\\Homeworks\\3 Python Homework\\Big Data Analysis using Python\\python-challenge\\PyBank\\budget_data.csv")
 
@@ -15,8 +18,18 @@ with open (flpth,newline="",encoding="UTF-8") as csvFile:
     for row in csvReader:
         month_count = month_count + 1
         total_amount = total_amount + int(row[1])
-        
+        profit_loss.append(float(row[1]))
+        dateList.append(row[0])
+
+    for x in range(1,len(profit_loss)):
+        average_chnge.append(profit_loss[x]-profit_loss[x-1])
+    
+    average_change= sum(average_chnge)/len(average_chnge)
+
 print("Financial Analysis")
 print("-----------------------------------------------------------")
 print(f"Total Months:  {month_count}")
-print(f"Total:  {total_amount}")
+print(f"Total: $ {total_amount}")
+print(f"Average  Change: $ {round(average_change,2)}")
+print(f"Greatest Increase in Profits:{dateList[(average_chnge.index(max(average_chnge)))+1]} (${max(average_chnge)})")
+print(f"Greatest Decrease in Profits:{dateList[(average_chnge.index(min(average_chnge)))+1]} (${min(average_chnge)})")
